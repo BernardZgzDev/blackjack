f8a1b5b1b82af07685ec896fce65c2ef863cbe97
Bernard_ZGZ_Dev <57059972+Bernard_ZGZ_Dev@users.noreply.github.com>
2021-06-01 13:35:58 +0200

    Commit 2021.06.01-JS

--- js/game.js
+++ js/game.js
@@ -1,218 +1,257 @@
-/*
-* 2C => Two of Clubs
-* 2D => Two of Diamonds
-* 2H => Two of Hearts
-* 2S => Two of Spades
-*/
+const blackjackGame = (() => {
 
-// Referencias HTML
-const playerPoints   = document.querySelector('#player-points');
-const computerPoints = document.querySelector('#computer-points');
-const playerDeck     = document.querySelector('#player-deck');
-const computerDeck   = document.querySelector('#computer-deck');
-const btnNuevo       = document.querySelector('#btnNuevo');
-const btnPedir       = document.querySelector('#btnPedir');
-const btnPlantarse   = document.querySelector('#btnPlantarse');
+    'use strict';
 
-const referencesHTML = {
-    'player-points' : playerPoints,
-    'computer-points' : computerPoints,
-    'player-deck' : playerDeck,
-    'computer-deck' : computerDeck,
-};
+    /*
+    * 2C => Two of Clubs
+    * 2D => Two of Diamonds
+    * 2H => Two of Hearts
+    * 2S => Two of Spades
+    */
 
-let deck       = [];
-const types    = ['H', 'D', 'C', 'S'];
-const specials = ['A', 'J', 'Q', 'K'];
-let puntosJugador = 0;
-let puntosComputadora = 0;
-let computerPlaying = false;
+    // Referencias HTML
+    const playersPoints  = document.querySelectorAll('.puntaje'),          
+          playersDeck    = document.querySelectorAll('.div-cartas-jugadores'),          
+          btnNuevo       = document.querySelector('#btnNuevo'),
+          btnPedir       = document.querySelector('#btnPedir'),
+          btnPlantarse   = document.querySelector('#btnPlantarse');
 
-btnNuevo.addEventListener('click', () => {
-    createDeck();
-});
-
-btnPedir.addEventListener('click', () => {
-    askCard( 'player-deck' );
-});
-
-btnPlantarse.addEventListener('click', () => {
-    turnoComputadora();
-});
-
-const createDeck = () => {
-
-    resetGame();
-
-    for ( let i = 2; i <= 10; i++ ) {
-
-        for ( let type of types ) {
-
-            deck.push( i + type );
-        }
-    }
-
-    for ( let special of specials ) {
-
-        for ( let type of types ) {
-
-            deck.push( special + type );
-        }
-    }   
-
-    deck = _.shuffle( deck );  
-        
-    setDisabled( btnPedir, false );
-    setDisabled( btnPlantarse, false );
-    setDisabled( btnNuevo, true );
-};
-
-const resetGame = () => {
-
-    puntosJugador = 0;
-    puntosComputadora = 0;    
-    deck = [];
-
-    let element = getElem( 'player-points' );
-    refreshUI( element, puntosJugador );
-
-    element = getElem( 'computer-points' );
-    refreshUI( element, puntosComputadora );
-
-    element = getElem( 'player-deck' );
-    removeCards( element );
-
-    element = getElem( 'computer-deck' );
-    removeCards( element );       
-};
-
-const getElem = ( type ) => {
-
-    return referencesHTML[type];
-};
-
-const refreshUI = ( element, points ) => {   
-
-    element.innerText = points;
-
-    if( puntosJugador === 0 ) {
-        setDisabled( btnPlantarse, true);
-
-    } else if ( puntosJugador >= 1 && puntosJugador < 21 ) {
-        setDisabled( btnPlantarse, false );
+    const referencesHTML = {
+        'players-points' : playersPoints,        
+        'players-deck' : playersDeck,        
+    };
     
-    } else if ( puntosJugador === 21 ) {
-        setDisabled( btnPlantarse, true ); 
-        setDisabled( btnPedir, true ); 
-        
-        if ( !computerPlaying ) {
-            computerPlaying = true;
-            turnoComputadora();       
+    const types    = ['H', 'D', 'C', 'S'],
+          specials = ['A', 'J', 'Q', 'K'];
+
+    let deck            = [],
+        puntosJugadores = [],        
+        computerPlaying = false;
+
+    /* Eventos */
+    btnNuevo.addEventListener('click', () => {
+        startGame();
+    });
+
+    btnPedir.addEventListener('click', () => {
+        askCard( 'players-deck', 0 );
+    });
+
+    btnPlantarse.addEventListener('click', () => {
+        turnoComputadora();
+    });
+
+    const startGame = ( numPlayers = 2 ) => {
+
+        resetGame( numPlayers );
+
+        for ( let i = 2; i <= 10; i++ ) {
+
+            for ( let type of types ) {
+
+                deck.push( i + type );
+            }
         }
 
-    } else {
+        for ( let special of specials ) {
+
+            for ( let type of types ) {
+
+                deck.push( special + type );
+            }
+        }   
+
+        deck = _.shuffle( deck );  
+            
+        setDisabled( btnPedir, false );
+        setDisabled( btnPlantarse, false );
+        setDisabled( btnNuevo, true );
+    };
+
+    const resetGame = ( numPlayers ) => {
+
+        puntosJugadores = [];
+
+        for ( let i = 0; i < numPlayers; i++ ) {
+            
+            puntosJugadores.push(0);
+        }
+        
+        deck = [];
+
+        let element = getElem( 'players-points', 0 );
+        refreshUI( element, puntosJugadores[0] );
+
+        element = getElem( 'players-points', puntosJugadores.length - 1 );
+        refreshUI( element, puntosJugadores[1] );
+
+        element = getElem( 'players-deck', 0 );
+        removeCards( element );
+
+        element = getElem( 'players-deck', puntosJugadores.length - 1 );
+        removeCards( element );       
+    };
+
+    const getElem = ( type, turno ) => {
+
+        return referencesHTML[type][turno];
+    };
+
+    const refreshUI = ( element, points ) => {   
+
+        element.innerText = points;
+
+        if( puntosJugadores[0] === 0 ) {
+            setDisabled( btnPlantarse, true);
+
+        } else if ( puntosJugadores[0] >= 1 && puntosJugadores[0] < 21 ) {
+            setDisabled( btnPlantarse, false );
+        
+        } else if ( puntosJugadores[0] === 21 ) {
+            setDisabled( btnPlantarse, true ); 
+            setDisabled( btnPedir, true ); 
+            
+            if ( !computerPlaying ) {
+                computerPlaying = true;
+                turnoComputadora();       
+            }
+
+        } else {
+            setDisabled(btnPedir, true);
+            setDisabled(btnPlantarse, true);
+            setDisabled(btnNuevo, false);
+
+            if ( !computerPlaying ) {
+                computerPlaying = true;
+                turnoComputadora();       
+            }            
+        }
+    };
+
+    const removeCards = ( element ) => {
+
+        while( element.hasChildNodes() ) {
+
+            element.removeChild(element.firstChild);
+        } 
+    };
+
+    const askCard = ( who, turno ) => {
+
+        if ( deck.length === 0 ) {
+            throw 'No cards in deck';
+        }
+
+        const card = deck.shift(),
+              cardValue = getCardValue( card ) * 1,
+              element = getElem( who, turno );
+
+        createCard( card, element );                
+        setPoints( cardValue, turno );    
+    };
+
+    const getCardValue = ( card ) => {
+
+        const value = card.substring( 0, card.length - 1 );
+        
+        return ( ['A'].includes(value) ) ? 11 :
+               ( ['J', 'Q', 'K'].includes(value) ) ? 10 : value;
+    };
+
+    const createCard = ( card, element ) => {
+        
+        const imgNode = document.createElement('img');
+        
+        imgNode.src = `assets/cards/${card}.png`;
+        imgNode.height = '240';
+        imgNode.classList.add('carta');
+
+        element.append(imgNode);
+    };
+
+    const setPoints = ( points, turno ) => {
+
+        if ( turno === 0 ) {
+            
+            let element  = getElem('players-points', turno);
+            
+            if( isNaN ( puntosJugadores[0] ) ) {
+                puntosJugadores[0] = points;    
+            
+            } else {
+                puntosJugadores[0] += points;
+
+            }           
+            refreshUI( element, puntosJugadores[0] );
+        
+        } else {
+
+            let element  = getElem('players-points', turno);
+
+            if( isNaN ( puntosJugadores[1] ) ) {
+                puntosJugadores[1] = points;    
+
+            } else {
+
+                puntosJugadores[1] += points;
+            }
+            
+            
+            refreshUI( element, puntosJugadores[1] );
+        }    
+    };
+
+    const setDisabled = ( btn, value ) => {
+        
+        btn.disabled = value;    
+    };
+
+    const turnoComputadora = () => {
+        
+        setDisabled(btnPedir, true);
+        setDisabled(btnPlantarse, true);        
+
+        let state = getWinner();
+
+        computerPlaying = false;
+
+        setTimeout( () => {
+            showMessage( state );
+        }, 100 );
+
+        setDisabled(btnNuevo, false);
         setDisabled(btnPedir, true);
         setDisabled(btnPlantarse, true);
-        setDisabled(btnNuevo, false);
+    };
 
-        if ( !computerPlaying ) {
-            computerPlaying = true;
-            turnoComputadora();       
-        }
-        
-    }
-};
+    const getWinner = () => {
 
-const removeCards = ( element ) => {
+        let state = '';
 
-    while( element.hasChildNodes() ) {
+        do {
+            askCard( 'players-deck', puntosJugadores.length - 1 );
 
-        element.removeChild(element.firstChild);
-    } 
-};
+            if( puntosJugadores[0] === 21 && puntosJugadores[1] === 21 ) {
+                state = 'tie';
+                break;            
+            }
 
-const askCard = ( who ) => {
+            if( puntosJugadores[0] > 21 || ( puntosJugadores[1] > puntosJugadores[0] && puntosJugadores[1] <= 21 ) ) {
+                state = 'lost';
+                break;            
+            }        
 
-    if ( deck.length === 0 ) {
-        throw 'No cards in deck';
-    }
+            if (puntosJugadores[1] > 21 ) {
+                state = 'won';
+                break;
+            }
 
-    const card = deck.shift();    
+        } while ( puntosJugadores[1] <= puntosJugadores[0] || puntosJugadores[1] === 21 );
 
-    const element = getElem( who );
-    
-    const imgNode = document.createElement('img');
-    
-    imgNode.src = `assets/cards/${card}.png`;
-    imgNode.height = '240';
-    imgNode.classList.add('carta');
+        return state;
+    };
 
-    element.append(imgNode);
-
-    cardValue = getCardValue( card ) * 1;
-    setPoints( who, cardValue );    
-};
-
-const getCardValue = ( card ) => {
-
-    const value = card.substring( 0, card.length - 1 );
-    
-    return ( ['A'].includes(value) ) ? 11 :
-           ( ['J', 'Q', 'K'].includes(value) ) ? 10 : value;
-};
-
-const setPoints = ( who, value ) => {
-
-    if ( who === 'player-deck' ) {
-        let element  = getElem('player-points');
-        
-        puntosJugador += value;
-        refreshUI( element, puntosJugador );
-    
-    } else {
-
-        let element  = getElem('computer-points');
-        
-        puntosComputadora += value;
-        refreshUI( element, puntosComputadora );
-    }    
-};
-
-const setDisabled = ( btn, value ) => {
-    
-    btn.disabled = value;    
-};
-
-const turnoComputadora = () => {
-
-    let state = '';
-
-    setDisabled(btnPedir, true);
-    setDisabled(btnPlantarse, true);
-
-    do {
-        askCard( 'computer-deck' );
-
-        if( puntosJugador === 21 && puntosComputadora === 21 ) {
-            state = 'tie';
-            break;            
-        }
-
-        if( puntosJugador > 21 || ( puntosComputadora > puntosJugador && puntosComputadora <= 21 ) ) {
-            state = 'lost';
-            break;            
-        }        
-
-        if (puntosComputadora > 21 ) {
-            state = 'won';
-            break;
-        }
-
-    } while ( puntosComputadora <= puntosJugador || puntosComputadora === 21 );
-
-    computerPlaying = false;
-
-    setTimeout( () => {
+    const showMessage = ( state ) => {
 
         if( state === 'won' ) {
 
@@ -237,16 +276,18 @@ const turnoComputadora = () => {
                 text: "You both got 21. It's a tie",
                 icon: "info",                                
             });
-        }
-        
-    }, 100 );
+        }        
+    };    
 
-    setDisabled(btnNuevo, false);
-    setDisabled(btnPedir, true);
-    setDisabled(btnPlantarse, true);
-};
+    const init = () => {
+        setPoints( 0, 0 );
+        setPoints( 0, 1 );
+        setDisabled( btnPedir, true);
+        setDisabled( btnPlantarse, true );
+    };
+    
+    return {
+        initGame : init 
+    }
 
-setPoints( 'player-deck', puntosJugador );
-setPoints( 'computer-deck', puntosComputadora );
-setDisabled( btnPedir, true);
-setDisabled( btnPlantarse, true );
+})();
